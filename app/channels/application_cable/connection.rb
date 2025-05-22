6fba259d878cd20b6b6baffe554df58667f797a5
Your Name <you@example.com>
2025-05-22 10:37:56 +0900

    2nd

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,3 +1,2 @@
 module ApplicationCable
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
 
-  #トップページのルーティング
-  get '/top', to: 'inkouresii#top'
+  # トップページのルーティング
+  get "/top", to: "inkouresii#top"
 
-  get 'inko', to: 'inkouresii#show', as: 'inko'
+  get "inko", to: "inkouresii#show", as: "inko"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
