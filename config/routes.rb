9dc83bb9c886f71ece675dcd6c9938d5062755fb
Your Name <you@example.com>
2025-05-22 11:49:53 +0900

    5th

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  #get "pages/top"
-  #get "posts/index"
+  # get "pages/top"
+  # get "posts/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
 
